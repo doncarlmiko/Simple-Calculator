256924abc7aa008c332e5b8960657f3dc0d591e8
doncarlmiko <konoha11061997@gmail.com>
2025-05-11 23:05:44 +0800

    Applied the input number field functionality to the calculator app.
    1. Added event listeners to the input number field to handle user input.
    2. Applied the input number field functionality on Task #002
    from Objectives 1 to 5.

--- js/index.js
+++ js/index.js
@@ -12,25 +12,53 @@ const numberDoubleZeroButton = document.querySelector('#doubleZero');*/
 
 const numberButtons = document.querySelectorAll('#one, #two, #three, #four, #five, #six, #seven, #eight, #nine, #zero, #doubleZero');
 
+const operatorButtons = document.querySelector('#add');
 
+let displayWithOperator = document.querySelector('#result1');
 let display = document.querySelector('#result2');
 let firstInput;
-//Function 
+let secondInput;
+let operator;
+let sum;
+
+
 numberButtons.forEach(button=>{
     button.addEventListener('click',()=>{
-        if(display.value ==='0'){
-            display.value = '';
+         // For first number input
+        if (operator === undefined) {
+            if (display.value === '0') {
+                display.value = '';
+            }
             display.value += button.value;
+            useFirstInput(display.value);
         }
-        else{
-             display.value += button.value;
+        // For second number input
+        else {
+            if (display.value === firstInput.toString()) {
+                display.value = ''; // Only clear when new number is clicked
+            }
+            display.value += button.value;
+            useSecondInput(display.value);
         }
-        useFirstInput(display.value);
     });
 });
 
-// Function to use firstInput
+// Function to get the first Input
 function useFirstInput(firstInputValue) {
-    firstInput = firstInputValue;
-    console.log('Current firstInput:', firstInput);
+    firstInput = parseFloat(firstInputValue);
+   console.log('Current firstInput:', firstInput);
 }
+
+operatorButtons.addEventListener('click',()=>{
+    operator = ' + ';
+    display.value = firstInput;
+    displayWithOperator.textContent = firstInput + operator;
+    console.log('Current operator:', operator);
+});
+
+// Function to get the second input
+function useSecondInput(secondInputValue) {
+    secondInput = parseFloat(secondInputValue);
+    console.log('Current secondInput:', secondInput);
+}
+
